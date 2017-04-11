ffd700d0bebf88c1f46a076f2e14758cf9aa77fb
Jeremías <jrmsdev@gmail.com>
2017-04-11 03:19:53 -0300

    no raw mode when --version

--- src/main.c
+++ src/main.c
@@ -7,23 +7,17 @@
 
 int
 main (int argc, char *argv[]) {
+	if (argc == 2 && strncmp (argv[1], "--version", 9) == 0)
+	{
+		write (STDOUT_FILENO, "mred v", 6);
+		write (STDOUT_FILENO, MRED_VERSION, strlen (MRED_VERSION));
+		write (STDOUT_FILENO, "\r\n", 2);
+		exit (0);
+	}
 	enable_raw_mode ();
 	mred_init ();
-	if (argc >= 2)
-	{
-		if (strncmp (argv[1], "--version", 9) == 0)
-		{
-			write (STDOUT_FILENO, "mred v", 6);
-			write (STDOUT_FILENO, MRED_VERSION,
-					strlen (MRED_VERSION));
-			write (STDOUT_FILENO, "\r\n", 2);
-			exit (0);
-		}
-		else
-		{
-			mred_open (argv[1]);
-		}
-	}
+	if (argc == 2)
+		mred_open (argv[1]);
 
 	mred_set_status_message (
 			"HELP: Ctrl-Q = quit | Ctrl-S = save | Ctrl-F = find");
